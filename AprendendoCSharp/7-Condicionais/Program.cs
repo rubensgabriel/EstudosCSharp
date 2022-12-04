d2a763f669b4f870bedf6d4fb0a44716ff4773df
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-12-04 20:28:28 -0300

    Criado estruturas condicionais

---
+++ AprendendoCSharp/7-Condicionais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        if (idadeJoao >= 18) 
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            if(quantidadePessoas > 0)
+            {
+                Console.WriteLine("Ele está acompanhando. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
