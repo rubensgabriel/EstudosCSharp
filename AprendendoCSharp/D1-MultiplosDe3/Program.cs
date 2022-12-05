8e4bd11fd2b3576c95ebe5ca6c2b81667820a08d
Rubens Gabriel <rubens-ita@hotmail.com.br>
2022-12-05 19:07:37 -0300

    finalização das aulas do curso 1

---
+++ AprendendoCSharp/D1-MultiplosDe3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        for (int contador = 3; contador <= 100; contador+=3)
+        {
+            Console.WriteLine(contador);
+        }
+    }
+}
---
+++ AprendendoCSharp/P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+        double investimento = 1000;
+
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            // investimento = investimento + investimento * 0.005;
+            investimento *= 1.005;
+            Console.WriteLine("No mês " + mes + " você tem R$ " + investimento);
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
